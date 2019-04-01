551cf68c68a9d18c40383beef67c2db74643fe95
wangjianbin <648188477@qq.com>
2019-04-01 16:57:38 +0800

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# jsTools
+a simple develop tool box for js
---
+++ tool.js
@@ -0,0 +1,47 @@
+class Path {
+    constructor(path) {
+        this.path = path
+    }
+
+    get(target, path) {
+        if (path === '?path') {
+            return this.path
+        } else if (path === '?slashedPath') {
+            return `${this.path}/`
+        } else if (!(path in target)) {
+            let p = ''
+            if (this.path) {
+                p = this.path
+            }
+            return new Proxy({}, new Path(`${p}/${path}`))
+        }
+    }
+}
+
+const PATH = new Proxy({}, new Path(""))
+
+const chainPath = function (path) {
+    return path['?path']
+}
+
+const chainSlashedPath = function (path) {
+    return path['?slashedPath']
+}
+
+/*
+> // a path which end without slash
+> chainPath(PATH.home)
+> "/home"
+> chainPath(PATH.book.history)
+> "/book/history"
+ 
+> // a path end with slash
+> 
+> chainSlashedPath(PATH.home)
+> "/home/"
+
+> // of course you can add pre path.
+> // eg. add a common pre path "/cms":
+> const PATH = new Proxy({}, new Path("/cms"))
+*/
+
